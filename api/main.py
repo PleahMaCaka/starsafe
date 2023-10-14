f89271ef0efb31d39239575f725be5b9b4c713eb
PleahMaCaka <pleahmacaka@gmail.com>
2023-10-14 13:09:51 +0900

    feat(`/file`): upload/download, get file list

--- api/main.py
+++ api/main.py
@@ -1,13 +1,27 @@
 import os
 
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
+
+from routes import file
+from utils import is_not_env_exist
+
+# Load env in dev-mode (directly run main.py in ./api dir)
+# 직접적으로 개발 환경에서 main.py를 실행하는 경우
+# .env 파일의 환경변수를 불러옴
+is_not_env_exist(load_dotenv)
+
+# 만약 docker-compose로 실행 한 경우 이미 환경변수가 존재하기에
+# load_dotenv()가 필요하지 않으므로 그냥 확인함
+is_not_env_exist()
+
+if not os.path.exists(f"..{file.SAFE_DIR}"):
+    os.mkdir(f"..{file.SAFE_DIR}")
 
 app = FastAPI()
 
-# app.include_router(file.router, prefix="/file", tags=["file"])
-
-PW = os.getenv("PW")
+app.include_router(file.router, prefix="/file", tags=["file"])
 
 
 @app.get("/")
@@ -15,10 +29,5 @@ def root():
     return {"message": "StarSafe is running!"}
 
 
-@app.get("/pw")
-def get_pw():
-    return {"pw": PW}
-
-
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", port=1777, reload=True)
--- api/routes/file.py
+++ api/routes/file.py
@@ -1,1 +1,49 @@
-from api.ai import yolo
+import os
+import shutil
+
+from fastapi import APIRouter, HTTPException
+from fastapi import File, UploadFile
+from fastapi.responses import FileResponse
+
+PASSWORD = os.getenv("PW")
+SAFE_DIR = os.getenv("SAFE_DIR")
+
+router = APIRouter()
+
+
+def check_password(pw: str):
+    if pw != PASSWORD:
+        raise HTTPException(status_code=401, detail="Incorrect password")
+
+
+@router.post("/upload")
+async def upload_file(pw: str, file: UploadFile = File(...)):
+    check_password(pw)
+
+    with open(f"..{SAFE_DIR}/{file.filename}", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {"message": "File upload successful"}
+
+
+@router.get("/")
+async def list_files(pw: str):
+    check_password(pw)
+
+    files = os.listdir(f"..{SAFE_DIR}")
+
+    if not files:
+        raise HTTPException(status_code=404, detail="Safe is empty")
+
+    return {"files": files}
+
+
+@router.get("/download")
+async def download_file(pw: str, file_name: str):
+    check_password(pw)
+
+    file_path = f"../safe/{file_name}"
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="File not found")
+
+    return FileResponse(path=file_path)
